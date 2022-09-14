9495305a9e7b3f47d766aca03a9a7e1b6c4313af
MuhammedBeraatKildaci <muhammedberaatkildaci@gmail.com>
2022-09-14 22:35:57 +0300

    14.09.2022 22:35 Java Yazılım Geliştirici Yetiştirme Kampı ilk ders

---
+++ intro/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String ortaMetin = "İlginizi çekebilir";
+        String altMetin = "Vade süresi";
+        System.out.println(ortaMetin);
+    }
+}
