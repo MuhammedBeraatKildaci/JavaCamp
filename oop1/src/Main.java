161f38e9957fa9e323467d9234d6048f9cb6fd7b
MuhammedBeraatKildaci <muhammedberaatkildaci@gmail.com>
2022-09-21 23:00:05 +0300

    21.09.2022 23:00

--- oop1/src/Main.java
+++ oop1/src/Main.java
@@ -1,13 +1,32 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-
         Product product1 = new Product();
-        product1.name = "Delonghi Kahve Makinesi";
-        product1.unitPrice = 7500;
-        product1.discount = 7;
-        product1.unitInStock = 3;
-        product1.imageUrl = "bilmemne.jpg";
+        product1.setName("Delonghi Kahve Makinesi");
+        product1.setUnitPrice(7500);
+        product1.setDiscount(7);
+        product1.setUnitInStock(3);
+        product1.setImageUrl("iamge1.jpg");
 
-        System.out.println(product1.name);
+        Product product2 = new Product();
+        product2.setName("Smeg Kahve Makinesi");
+        product2.setUnitPrice(6500);
+        product2.setDiscount(8);
+        product2.setUnitInStock(2);
+        product2.setImageUrl("iamge2.jpg");
+
+        Product product3 = new Product();
+        product3.setName("Kitchen Aid Kahve Makinesi");
+        product3.setUnitPrice(4500);
+        product3.setDiscount(9);
+        product3.setUnitInStock(4);
+        product3.setImageUrl("iamge3.jpg");
+
+        Product[] products = { product1, product2, product3 };
+
+        System.out.println("<ul>");
+        for (var product : products) {
+            System.out.println("<li>" + product.getName() + "</li>");
+        }
+        System.out.println("</ul>");
     }
 }
--- oop1/src/Product.java
+++ oop1/src/Product.java
@@ -1,7 +1,56 @@
 public class Product {
-    String name;
-    double unitPrice;
-    double discount;
-    String imageUrl;
-    int unitInStock;
+    private String name;
+    private double unitPrice;
+    private double discount;
+    private String imageUrl;
+    private int unitInStock;
+
+    // public Product(String name, double unitPrice, double discount, String
+    // imageUrl, int unitInStock) {
+    // this.name = name;
+    // this.unitPrice = unitPrice;
+    // this.discount = discount;
+    // this.imageUrl = imageUrl;
+    // this.unitInStock = unitInStock;
+    // }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getUnitInStock() {
+        return unitInStock;
+    }
+
+    public void setUnitInStock(int unitInStock) {
+        this.unitInStock = unitInStock;
+    }
 }
