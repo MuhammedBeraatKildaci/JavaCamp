0947f00b656db7ac73427a23f3f23223b17e0547
Muhammed Beraat Kildaci <muhammedberaatkildaci@gmail.com>
2023-02-26 19:22:30 +0300

    spring boot started rent a car project

---
+++ rent_a_car/src/main/java/kodlama/io/rent_a_car/entities/concrates/Brand.java
@@ -0,0 +1,23 @@
+package kodlama.io.rent_a_car.entities.concrates;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="brand_id")
+    private int id;
+    @Column(name="brand_name")
+    private String name;
+}
